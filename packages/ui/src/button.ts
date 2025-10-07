3a22cef76c867074e68fb5cc5e1da5a0851137e5
martinrossil <martinrossil@gmail.com>
2025-10-07 18:04:31 +0200

    fix: testing NPM publish

--- packages/ui/src/button.ts
+++ packages/ui/src/button.ts
@@ -3,7 +3,7 @@ import { Component } from '@freax/core/component';
 export class Button extends Component {
 	public constructor() {
 		super();
-		console.log('Button3');
+		console.log('Button v4');
 	}
 }
 customElements.define('fx-button', Button);
